b2ed9e720ff0751503695145d861163a9f31b444
Trevor Harmon <trevorharmon@Trevors-MacBook-Pro.local>
2021-12-06 16:44:49 -0500

    Minor update

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 - [zsh-autosuggestions](https://github.com/zsh-users/zsh-autosuggestions)
 - [Powerlevel10k](https://github.com/romkatv/powerlevel10k)
 - [Input Mono](https://input.djr.com/)
+- [Smyck color theme](http://color.smyck.org/)
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,31 @@
+# install oh-my-zsh via curl
+echo "📥  Downloading & installing Oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# grab zsh-autosuggestions
+echo "📥  Downloading & installing zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Clone the powerlevel10k theme to the appropriate place
+echo "📥  Downloading & installing powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+  # copy the zshrc file
+  echo "⚙️  Setting up shell aliases"
+  cp zshrc ~/.zshrc
+
+  # Copy powerlevel10k config to the appropriate place
+  echo "⚙️  Setting up zsh theme config"
+  cp p10k.zsh ~/.p10k.zsh
+
+  # copy the theme
+  echo "⚙️  Setting up zsh theme"
+  cp powerlevel10k.zsh-theme ~/powerlevel10k/powerlevel10k.zsh-theme
+fi
+
 if [[ "$SPIN" = 1 ]]
 then
-  # install oh-my-zsh via curl
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-  # grab zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-  # Clone the powerlevel10k theme to the appropriate place
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-
   # Copy powerlevel10k config to the appropriate place
   ln -sf ~/dotfiles/p10k.zsh ~/.p10k.zsh
 
